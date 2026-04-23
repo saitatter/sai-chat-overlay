42b67af3b41999fe3af02e0bb71480ddd2a1e17e
SaiTatter <andrei.voicu133@gmail.com>
2026-04-23 15:36:11 +0300

    fix(release): treat ci/chore/refactor commits as patch

--- release.config.js
+++ release.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { type: "refactor", release: "patch" },
+          { type: "ci", release: "patch" },
+          { type: "chore", release: "patch" }
+        ]
+      }
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/github"
   ]
